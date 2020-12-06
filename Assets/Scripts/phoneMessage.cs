171329b2a201f3cc69bfa27f9edca04e0d0c79dd
wengy12 <wengy12>
2020-12-06 16:23:29 -0500

    inintial commit

---
+++ Assets/Scripts/phoneMessage.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class phoneMessage : MonoBehaviour
+{
+
+    public int messageCount = 1;
+
+    private TMP_Text m_TextComponent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_TextComponent = GetComponent<TMP_Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_TextComponent.text = "Phone(" + messageCount + ")";
+    }
+}
